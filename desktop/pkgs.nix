4b23de97774b4cce3c7886be0f2a1004ca1d8102
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-03-05 20:03:09 +0100

    Add desktop pkgs

--- desktop/pkgs.nix
+++ desktop/pkgs.nix
@@ -2,6 +2,22 @@
 
 {
   environment.systemPackages = with pkgs; [
-    alsamixer
+    arandr                   
+    cdrkit                   # CD/DVD utilities.
+    chromium
+    evince
+    ffmpeg
+    keepass
+    libreoffice
+    networkmanagerapplet
+    openjdk8
+    pavucontrol           
+    rofi                     # A window switcher + run dialogue (substitutes dmenu).
+    texLive
+    texLiveBeamer
+    visualvm
+    (wine.override { pulseaudioSupport = true; })
+    winetricks
+    xbanish                  # Hide a cursor when typing.
   ];
 }
