4d1679d36ab9f3711adff17b1a37a3979f5dacdd
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-04-06 12:02:32 +0000

    Add a warning comment over the grsecurity option

--- desktop/system.nix
+++ desktop/system.nix
@@ -5,5 +5,8 @@
   
   # A suite of patches that make exploiting kernel bugs
   # more difficult.
+  # This is a potential cause of various compatibility issues,
+  # so bear in mind that in such cases this is the first place to 
+  # look at.
   security.grsecurity.enable = true;
 }
