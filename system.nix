e5fb9c09f8e8b71e95a03fe77f4fb68f69060d21
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-07-24 16:39:44 +0200

    Fix typo in the comment on system.nix

--- system.nix
+++ system.nix
@@ -1,10 +1,10 @@
 { ... }:
 
-{ 
+{
 
-  # The maximum number of derivations that will be build in parallel.
+  # The maximum number of derivations that will be built in parallel.
   nix.maxJobs = 4;
-  
+
   # Remember to disable it:
-  nixpkgs.config.allowBroken = true; 
+  nixpkgs.config.allowBroken = true;
 }
