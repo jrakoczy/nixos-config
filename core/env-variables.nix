558b4c21717c0ad393b2273df8ceafacf0a4b929
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2017-08-27 23:50:40 +0200

    Add jrakoczy's custom nixpkgs to NIX_PATH

--- core/env-variables.nix
+++ core/env-variables.nix
@@ -1,12 +1,13 @@
 { ... }:
 
 {
-  environment.variables.PATH = [ "$HOME/.bin" ];  
+  environment.variables.PATH = [ "$HOME/.bin" ];
 
   # We need to set NIX_PATH explicitly to make sure we have a full control
   # over Nix evaluator's access permissions (see: nix.extraOptions:restrict-eval).
   nix.nixPath = [
       "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos/nixpkgs"
       "nixos-config=/etc/nixos/configuration.nix:/etc/nixos:/nix/var/nix/profiles/per-user/root/channels"
+      "jrakoczy-pkgs=https://github.com/jrakoczy/aux-nixpkgs/archive/master.tar.gz"
   ];
 }
--- core/system.nix
+++ core/system.nix
@@ -9,37 +9,37 @@ let
   nixosVersion = "17.03";
 in
 
-{ 
+{
   system = {
 
     autoUpgrade = {
-    
+
       # A systemd service that runs nixos-rebuild switch --upgrade.
       enable = true;
 
-      # Auto-upgrade time 
-      dates = maintenanceTime; 
+      # Auto-upgrade time
+      dates = maintenanceTime;
 
       # Define explicitly.
       channel = "https://nixos.org/channels/nixos-${nixosVersion}";
     };
-   
+
     # Keep full control over package versions.
     # If we don't define it explicitly, it may turn out that we upgraded
-    # some packaged to a version that is not backward compatible. 
+    # some packaged to a version that is not backward compatible.
     stateVersion = nixosVersion;
   };
 
   nix = {
-   
+
     # The maximum number of build tasks that will be executed in parallel.
     # The value is passed as make's command -j switch.
     buildCores = 0;
- 
+
     # build-cache-failure -- remember that a given build has failed.
-    # auto-optimise-store -- hardlinks instead of duplicate store files. 
+    # auto-optimise-store -- hardlinks instead of duplicate store files.
     # restrict-eval -- Nix evaluator doesn't have an access to files
-    #                  outside the Nix search path (make sure that 
+    #                  outside the Nix search path (make sure that
     #                  NIX_PATH is defined appropriately).
     extraOptions = ''
       auto-optimise-store = true
@@ -48,14 +48,14 @@ in
 
     # Prevent from accessing dependencies from outside of the Nix store.
     useSandbox = true;
-    
+
     # Clean up old, unreferenced packages.
     gc = {
       automatic = true;
       dates = maintenanceTime;
-      
-      # Delete old and unused profile generations. 
-      options = "--delete-older-than 60d"; 
+
+      # Delete old and unused profile generations.
+      options = "--delete-older-than 60d";
     };
   };
 }
